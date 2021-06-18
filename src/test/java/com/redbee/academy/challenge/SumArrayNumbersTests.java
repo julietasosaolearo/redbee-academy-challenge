c3135e1246a65615feadbe458db93ee8766aafca
julietasosaolearo <86113218+julietasosaolearo@users.noreply.github.com>
2021-06-18 18:06:48 -0300

    Update SumArrayNumbersTests.java

--- src/test/java/com/redbee/academy/challenge/SumArrayNumbersTests.java
+++ src/test/java/com/redbee/academy/challenge/SumArrayNumbersTests.java
@@ -1,29 +1,51 @@
 package com.redbee.academy.challenge;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import java.util.Scanner;
+import java.util.Arrays;
+public class TwoArraySum {
+  public static void main(String[] args) {
 
-import java.util.Collections;
-import java.util.List;
+    
+    Scanner scan = new Scanner(System.in);
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+    
+    System.out.print("Enter numbers of first array: ");
+    int array1size = scan.nextInt();
+    System.out.print("Enter numbers of second array: ");
+    int array2size = scan.nextInt();
 
-public class SumArrayNumbersTests {
+    
+    if(array1size != array2size) {
+      System.out.println("Both array must have "+
+                      "same number of elements");
+      return; // stop 
+    }
 
-  @Test
-  @DisplayName("Tests happy path")
-  public void testHappyPath() {
-    Integer result = SumArrayNumbers.sum(List.of(1, 2, 3));
-    assertNotNull(result, "The result cannot be null");
-    assertEquals(6, result);
-  }
+   
+    int array1[] = new int[array1size];
+    int array2[] = new int[array1size];
+    int array3[] = new int[array1size];
 
-  @Test
-  @DisplayName("Test empty list")
-  public void testEmpty() {
-    Integer result = SumArrayNumbers.sum(Collections.emptyList());
-    assertNotNull(result, "The result cannot be null");
-    assertEquals(0, result);
+    
+    System.out.println("Enter first array: ");
+    for (int i=0; i<array1.length; i++) {
+      array1[i] = scan.nextInt();
+    }
+    
+    System.out.println("Enter second array: ");
+    for (int i=0; i<array2.length; i++) {
+      array2[i] = scan.nextInt();
+    }
+
+    
+    for (int i=0; i<array3.length; i++) {
+      
+      array3[i] = array1[i] + array2[i];
+    }
+
+    
+    System.out.println("Resultant Array: " 
+        + Arrays.toString(array3));
   }
 }
+
